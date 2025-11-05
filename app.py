c7e78b6ec2004bf2f754d0437f05b6d53a6ce616
loic025 <loic.jeancler@ac-normandie.fr>
2025-11-05 14:40:33 -0300

    Update app.py
    Modif port

--- app.py
+++ app.py
@@ -77,5 +77,7 @@ def static_files(filename):
 
 
 if __name__ == '__main__':
-    # Utile en local uniquement. Sur Render, c'est gunicorn qui lance l'app.
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
